5f93bfc0bc12390b0b8275c5eeba116b1ac4f4f8
unknown <penisxuei@gmail.com>
2024-12-16 09:41:43 +0700

    fix health

--- controllers/loadBalancerController.mjs
+++ controllers/loadBalancerController.mjs
@@ -24,21 +24,33 @@ export async function balanceRequest(req, res, next) {
     try {
         const server = await getAvailableServer();
         const targetUrl = `http://${server.address}${req.url}`;
-        
+
+        console.log(`Епта метод ${req.method} request to ${targetUrl}`);
+        // Копируем тело запроса (если требуется)
+        const body =
+            ['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method.toUpperCase()) && req.body
+                ? JSON.stringify(req.body)
+                : undefined;
+
+        // Параметры запроса
         const options = {
             method: req.method,
             headers: {
                 ...req.headers,
                 'Content-Type': req.headers['content-type'] || 'application/json',
             },
-            body: ['POST', 'PUT', 'PATCH'].includes(req.method) ? JSON.stringify(req.body) : undefined,
+            body,
         };
 
+        // Перенаправляем запрос
         const response = await fetch(targetUrl, options);
 
+        // Читаем данные ответа
         const responseData = await response.text();
+
+        // Отправляем ответ клиенту
         res.status(response.status).send(responseData);
-        console.log(`Forwarded request to ${targetUrl} with status ${response.status}`);
+        console.log(`Forwarded ${req.method} request to ${targetUrl} with status ${response.status}`);
     } catch (error) {
         console.error('Error forwarding request:', error.message);
         res.status(500).send(`Unable to forward request: ${error.message}`);
