d63edecb52a140fd5ce13dd5b54fa88f7c807d50
unknown <penisxuei@gmail.com>
2025-01-16 18:05:38 +0700

    add admin aser all

--- controllers/admin.mjs
+++ controllers/admin.mjs
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import User from '../models/User.mjs';
 import Role from '../models/Role.mjs';
 import Task from '../models/Task.mjs';
@@ -7,23 +8,12 @@ import DailyCombo from '../models/DailyCombo.mjs';
 
 export const userAll = async (req, res) => {
     try {
-        const users = await User.findAll({
-            include: [
-                {
-                    model: Role,
-                    as: 'role',
-                    attributes: ['name'],
-                },
-            ],
-        });
-        //посчитать сколько всегро пользователей, сколько пригласили людей и сколько из них подключили кошелек
-        const user_count = users.length;
-        const invited_count = users.filter(user => user.Invited).length;
-        const invited_wallet_count = users.filter(user => user.Invited && user.wallet).length;
-        const invited_wallet_percent = invited_wallet_count / invited_count * 100;
-        const invited_percent = invited_count / user_count * 100;
-        res.json({ users, user_count, invited_count, invited_wallet_count, invited_wallet_percent, invited_percent });
-
+        const user_count = await User.count();
+        const invited_count = await User.count({ where: { Invited: { [Op.not]: null } } });
+        const wallet_count = await User.count({ where: { wallet: { [Op.not]: null } } });
+        
+        res.json({ user_count, invited_count, wallet_count });
+        
     } catch (error) {
         console.error('Database error:', error);
         res.status(500).json({ message: 'Internal server error' });
